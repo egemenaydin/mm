9a68a33f2083aa10e5ac222507e384b3204570ef
Egemen Aydin <egemenaydin1@gmail.com>
2014-08-18 22:17:16 -0500

    log 2 calculation correction

--- volcano-plot.R
+++ volcano-plot.R
@@ -1,17 +1,17 @@
-data <- read.csv("511-log-2-triplicates-nocomp.csv")
+data <- read.csv("trial.csv")
 
 data[data == 0] <- 0.00000000001
 
-data.frame(data[1:2991, ], data$p <- apply(data[1:2991, ], 1, function(x) {
+data.frame(data[1:2376, ], data$p <- apply(data[1:2376, ], 1, function(x) {
         t.test(x[1:3], x[4:6], paired = TRUE)$p.value
 } ))
 
-data.frame(data[1:2991, ], data$log_p <- apply(data[1:2991, ], 1, function(y){
+data.frame(data[1:2376, ], data$log_p <- apply(data[1:2376, ], 1, function(y){
         -log10(y[7])
 }))
 
-data.frame(data[1:2991, ], data$dif <- apply(data[1:2991, ], 1, function(z){
-        mean(z[1:3] - z[4:6], na.rm = TRUE)
+data.frame(data[1:2376, ], data$dif <- apply(data[1:2376, ], 1, function(z){
+        log2(mean(z[1:3], na.rm = TRUE) / mean(z[4:6], na.rm = TRUE))
 }))
 
 data2 <- read.csv("511-log-2-triplicates.csv")
