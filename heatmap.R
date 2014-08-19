7334805a94bc6e52527468d54c279129cfc66a67
Egemen Aydin <egemenaydin1@gmail.com>
2014-08-18 23:45:26 -0500

    color brewer

--- heatmap.R
+++ heatmap.R
@@ -1,12 +1,13 @@
 require(gplots)
+require(RColorBrewer)
 
-data <- read.csv("air-sparge-all-log-2-no-mq.csv")
+data <- read.csv("air-sparge-S-log-2-no-mq.csv")
 rownames(data) <- make.names(data[, 1], unique = TRUE)
 data$Compound <- NULL
 
-pdf("heatmap-all-5.pdf")
+pdf("heatmap-S.pdf")
 heatmap.2(as.matrix(data), Rowv = FALSE, Colv = TRUE, 
-          dendrogram = "column", col = redblue(16), 
+          dendrogram = "column", col = brewer.pal(8, "Reds"), 
           key = T, keysize = 1.5, density.info = "none", 
           trace = "none", labRow = NA)
 dev.off()
