f0e37c466f129ce0478c23714c17a34f57edb3a1
egemenaydin <egemenaydin1@gmail.com>
2014-08-21 17:27:33 -0500

    printer polished

--- heatmap.R
+++ heatmap.R
@@ -1,13 +1,13 @@
 require(gplots)
 require(RColorBrewer)
 
-data <- read.csv("air-sparge-S-log-2-no-mq.csv")
+data <- read.csv("camelina-all-log2.csv")
 rownames(data) <- make.names(data[, 1], unique = TRUE)
 data$Compound <- NULL
 
-pdf("heatmap-S-new-2.pdf")
+
 heatmap.2(as.matrix(data), Rowv = FALSE, Colv = TRUE, 
           dendrogram = "column", col = bluered(16), 
           key = T, keysize = 1.5, density.info = "none", 
           trace = "none", labRow = NA)
-dev.off()
+dev.print(pdf, "heatmap-S-new-2.pdf", height = 6, width = 6)
