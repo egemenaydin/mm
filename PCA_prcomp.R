bb11ed84b90fda26ade0a100c6ba833e2e5a2597
egemenaydin <egemenaydin1@gmail.com>
2014-11-06 13:37:46 -0600

    converted to ggplot

--- PCA_prcomp.R
+++ PCA_prcomp.R
@@ -1,16 +1,29 @@
-library(ggbiplot)
+library(ggplot2)
 data <- read.csv("sample2.csv", header = TRUE, check.names = FALSE)
+data2 <- read.csv("sample2.csv", header = TRUE, check.names = FALSE)
 rownames(data) <- make.names(data$Compound, unique = TRUE)
 data$Compound <- NULL
-data[data == 0] <- 2
 
-log.data <- log(data[ , 2:length(data)])
-samples <- data$Compound
+data_polished <- data[, apply(data, 2, var, na.rm = TRUE) != 0]
+data_polished[data_polished == 1] <- 0.5*(min(data_polished[data_polished>0],na.rm=TRUE))
+
+log.data <- log(data_polished[ , 2:length(data_polished)])
+
+Samples <- data2$Compound
 pca <- prcomp(log.data, center = TRUE, scale. = TRUE)
-plot(pca, type = "l")
-summary(pca)
-print(pca)
-x <- predict(pca)
-y <- pca$rotation
-g <- ggbiplot(pca, obs.scale = 1, var.scale = 1, groups = samples, ellipse = TRUE, circle = TRUE, var.axes = FALSE)
-print(g)
+
+PC <- predict(pca)
+x <- write.csv(PC, file = "PCA.csv")
+y <- read.csv("PCA.csv", row.names = 1)
+summary <- summary(pca)
+loadings <- pca$rotation
+
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+xlab <- sprintf ('PC1 (%0.1f%%)', 100*pca$sdev[1]^2/sum(pca$sdev^2))
+
+g <- ggplot(y, aes(PC1, PC2))
+g + geom_point(aes(color = Samples), size = 3) + theme_bw(base_size = 12)
+
+
+dev.print(pdf, file = "PCA.pdf", height=7, width=11)
