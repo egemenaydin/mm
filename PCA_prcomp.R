0d110ff536daa603382b7cb170939373e4ce62f5
egemenaydin <egemenaydin1@gmail.com>
2014-11-06 16:46:36 -0600

    loading graph added

--- PCA_prcomp.R
+++ PCA_prcomp.R
@@ -14,17 +14,25 @@ Samples <- data2$Compound
 pca <- prcomp(log.data, center = TRUE, scale. = TRUE)
 
 PC <- predict(pca)
-x <- write.csv(PC, file = "PCA.csv")
-y <- read.csv("PCA.csv", row.names = 1)
+write.csv(PC, file = "PCA.csv")
+x <- read.csv("PCA.csv", row.names = 1)
 summary <- summary(pca)
 loadings <- pca$rotation
+
+write.csv(loadings, file="loadings.csv")
+y <- read.csv("loadings.csv", row.names = 1)
 
 my.palette <- c("#000000","#831E3F", "#38D750", "#779BFB", "#7E6E0B", "#399EA4", "#FEFF89", "#ED7028", "#E2B5C9", "#3AEBB8", "#443B86", "#F85093", "#C6FC3C", "#2E5847", "#9A0D0F", "#F6535F", "#E0A7F5", "#603B0F", "#206CCE", "#5C6D05")
 
 x_lab <- sprintf ('PC1 (%0.1f%%)', 100*pca$sdev[1]^2/sum(pca$sdev^2))
 y_lab <- sprintf ('PC2 (%0.1f%%)', 100*pca$sdev[2]^2/sum(pca$sdev^2))
 
-g <- ggplot(y, aes(PC1, PC2))
+g <- ggplot(x, aes(PC1, PC2))
 g + geom_point(aes(color = Samples), size = 5) + theme_bw(base_size = 12) + xlab(x_lab) + ylab(y_lab) + scale_colour_manual(values = my.palette)
 
 dev.print(pdf, file = "PCA.pdf", height=7, width=11)
+
+d <- ggplot(y, aes(PC1, PC2))
+d + geom_point(size = 2) + theme_bw(base_size = 12) + xlab(x_lab) + ylab(y_lab)
+
+dev.print(pdf, file = "loadings.pdf", height=7, width=11)
