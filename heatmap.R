bf1294ede0d7847fcd480163bc88f10600294f18
egemenaydin <egemenaydin1@gmail.com>
2014-11-08 15:08:16 -0600

    colors were changed to reds

--- heatmap.R
+++ heatmap.R
@@ -1,12 +1,12 @@
 require(gplots)
 require(RColorBrewer)
 
-data <- read.csv("PoE-log2-S-nonaveraged.csv", check.names = FALSE)
+data <- read.csv("raw-data-log2-below-400-no-mass.csv", check.names = FALSE)
 rownames(data) <- make.names(data[, 1], unique = TRUE)
 data$Compound <- NULL
 
 heatmap.2(as.matrix(data), Rowv = FALSE, Colv = TRUE, 
-          dendrogram = "column", col = bluered(16), 
+          dendrogram = "column", col = brewer.pal(8, "Reds"), 
           key = T, key.title = NULL, keysize = 1.2, density.info = "none", 
           trace = "none", labRow = NA)
-dev.print(pdf, "heatmap-S.pdf", height = 5, width = 10)
+dev.print(pdf, "heatmap-below-400.pdf", height = 5, width = 10)
