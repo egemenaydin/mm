c3b1959c570a75b003b0d43d1e25d709faa03ad4
egemenaydin <egemenaydin1@gmail.com>
2014-11-08 14:14:55 -0600

    font was changed to Times New Roman

--- mass-vs-fold-change.R
+++ mass-vs-fold-change.R
@@ -38,6 +38,7 @@ data.frame(fold1, fold1$fold_change <- apply(fold1, 1, function(x){
         log2(x[11]) - log2(x[8])
 }))
 
+windowsFonts(Times=windowsFont("TT Times New Roman"))
 
 g <- ggplot(fold1, aes(fold_change, Mass))
-g + geom_point(aes(color = "Marooon"), size = 3) + theme_bw(base_size = 16) + geom_abline(intercept = 400, slope = 0, color = "darkgreen", size = 2) + geom_vline(xintercept = 0, color = "darkgreen", size = 2) + theme(legend.position = "none")
+g + geom_point(aes(color = "Marooon"), size = 3) + theme_bw(base_size = 16, base_family = "Times") + geom_abline(intercept = 400, slope = 0, color = "darkgreen", size = 2) + geom_vline(xintercept = 0, color = "darkgreen", size = 2) + theme(legend.position = "none")
