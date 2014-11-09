4c434830b5a644e00829ce6f51c40df21869a8e1
Egemen Aydin <egemenaydin1@gmail.com>
2014-11-09 00:19:32 -0600

    start

---
+++ x-y-plot-2.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+data <- read.csv("sample.csv")
+
+data[data == 0] <- NA
+
+windowsFonts(Times=windowsFont("TT Times New Roman"))
+
+my.palette <- c("#000000", "#F0E442", "#56B4E9", "#009E73", "#D55E00", "#0072B2", "#CC79A7")
+
+ggplot(data, aes(x = Sample, y = Abundance)) + 
+        scale_color_manual(values = my.palette) +
+        geom_point(size = 5, color = "black", shape = 15) + 
+        geom_point(size = 3, aes(color = Category), shape = 3) +
+        guides(colour = guide_legend(override.aes = list(shape = 15, size = 5))) +
+        theme_bw(base_size =16, base_family = "Times") + 
+        geom_errorbar(aes(ymax = Abundance + Sdev, ymin = Abundance - Sdev), width = 0.25) + 
+        theme(axis.text.x = element_text(angle = 90, hjust = -0.25, vjust = 0.5)) + 
+        scale_x_discrete(limits = (data$Sample)[order(data$Category)]) + 
+        annotate("text", x = 0.75, y = 40000, label = "Not detected", angle = 90, size = 7, family = "Times") +
+        annotate("text", x = 4:6, y = 40000, label = "Not detected", angle = 90, size = 7, family = "Times") + 
+        annotate( "text", x = 8, y = 40000, label = "Not detected", angle = 90, size = 7, family = "Times") + 
+        annotate( "text", x = 11:14, y = 40000, label = "Not detected", angle = 90, size = 7, family = "Times") + 
+        geom_rect(data=NULL,aes(xmin=0.25,xmax=1.5,ymin=-Inf,ymax=Inf), fill="#000000", alpha = 0.05) +
+        geom_rect(data=NULL,aes(xmin=1.5,xmax=2.5,ymin=-Inf,ymax=Inf), fill="#F0E442", alpha = 0.05) +
+        geom_rect(data=NULL,aes(xmin=2.5,xmax=6.5,ymin=-Inf,ymax=Inf), fill="#56B4E9", alpha = 0.05) +
+        geom_rect(data=NULL,aes(xmin=6.5,xmax=10.5,ymin=-Inf,ymax=Inf), fill="#009E73", alpha = 0.05) +
+        geom_rect(data=NULL,aes(xmin=10.5,xmax=14.75,ymin=-Inf,ymax=Inf), fill="#D55E00", alpha = 0.05)        
+
+dev.print(pdf, file = "sulfolane-abundance-change.pdf", height=8, width=10)
