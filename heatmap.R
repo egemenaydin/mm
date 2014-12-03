1dbe8dd297bee97d0a4058adedaff76374e149b8
egemenaydin <egemenaydin1@gmail.com>
2014-12-03 11:08:56 -0600

    log2 and polished data

--- heatmap.R
+++ heatmap.R
@@ -1,14 +1,19 @@
 require(gplots)
 require(RColorBrewer)
 
-data <- read.csv("air-sparge-all-raw-mass-ordered-no-mass-log2.csv", check.names = FALSE)
+data <- read.csv("sample2-transposed.csv", check.names = FALSE)
 rownames(data) <- make.names(data[, 1], unique = TRUE)
 data$Compound <- NULL
-log.data <- data.frame(log2(data[ , 1:length(data)]), check.names = FALSE)
+
+data_polished <- data[, apply(data, 2, var, na.rm = TRUE) != 0]
+data_polished[data_polished == 0] <- 1
+
+log.data <- data.frame(log2(data_polished[ , 1:length(data)]), check.names = FALSE)
+
 
 cols <- colorRampPalette(c("white","red"))(16)
 
-heatmap.2(as.matrix(data), Rowv = FALSE, Colv = TRUE, 
+heatmap.2(as.matrix(log.data), Rowv = FALSE, Colv = TRUE, 
           dendrogram = "column", col = cols, 
           key = T, key.title = NULL, keysize = 2, density.info = "none", 
           trace = "none", labRow = NA)
