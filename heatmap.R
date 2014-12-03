d9fbdbdf0d6ecc5e29c930480b30b7c71f424f5c
egemenaydin <egemenaydin1@gmail.com>
2014-12-03 12:03:00 -0600

    scale added color range changed to 75 from black to red

--- heatmap.R
+++ heatmap.R
@@ -11,10 +11,10 @@ data_polished[data_polished == 0] <- 1
 log.data <- data.frame(log2(data_polished[ , 1:length(data)]), check.names = FALSE)
 
 
-cols <- colorRampPalette(c("white","red"))(16)
+cols <- colorRampPalette(c("black","red"))(75)
 
-heatmap.2(as.matrix(log.data), Rowv = FALSE, Colv = TRUE, 
-          dendrogram = "column", col = cols, 
+heatmap.2(as.matrix(data), Rowv = TRUE, Colv = TRUE, 
+          dendrogram = "column", col = cols, scale = c("row"),
           key = T, key.title = NULL, keysize = 2, density.info = "none", 
           trace = "none", labRow = NA)
 dev.print(pdf, "heatmap-below-400.pdf", height = 5, width = 10)
