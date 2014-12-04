a2fa5038261e4161b6f18592abeb0a25b1b7633b
egemenaydin <egemenaydin1@gmail.com>
2014-12-04 13:08:45 -0600

    explanation added

--- PCA_prcomp.R
+++ PCA_prcomp.R
@@ -22,8 +22,10 @@ loadings <- pca$rotation
 write.csv(loadings, file="loadings.csv")
 y <- read.csv("loadings.csv", row.names = 1)
 
+#20 color palette
 my.palette <- c("#000000","#831E3F", "#38D750", "#779BFB", "#7E6E0B", "#399EA4", "#FEFF89", "#ED7028", "#E2B5C9", "#3AEBB8", "#443B86", "#F85093", "#C6FC3C", "#2E5847", "#9A0D0F", "#F6535F", "#E0A7F5", "#603B0F", "#206CCE", "#5C6D05")
 
+#26 color palette
 my.palette <- c("#FE39CD", "#79B609", "#17B9FB", "#C82519", "#122818", "#E9CBBD", "#FBB34B", "#671B62", "#00C684", "#711624", "#4A86FC", "#1E6015", "#F9366A", "#FF94B3", "#E8E207", "#2DE3F1", "#126C58", "#FADE8F", "#A9E9AA", "#A16C0B", "#241720", "#36A5B5", "#239D25", "#AC0337", "#F56828", "#501513")
 
 x_lab <- sprintf ('PC1 (%0.1f%%)', 100*pca$sdev[1]^2/sum(pca$sdev^2))
