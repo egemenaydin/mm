64d6e60f1b287674172d2340ef3ab7ae8e29719a
egemenaydin <egemenaydin1@gmail.com>
2015-05-11 15:27:58 -0500

    added facet grid, removed mean

--- boxplot.R
+++ boxplot.R
@@ -1,11 +1,15 @@
 library(ggplot2)
-data <- read.csv("boxplot.csv")
-means <- aggregate(Recovery ~ Matrix, data, mean)
-ggplot(data, aes(x = Matrix, y = Recovery, fill = Matrix)) +
+data <- read.csv("AKO1-all.csv")
+means <- aggregate(Abundance ~ Peak, data, mean)
+options(scipen=1000000)
+ggplot(data, aes(x = Peak, y = Abundance, fill = Peak)) +
         geom_boxplot() +
-        geom_point(size = 3, show_guide = FALSE) +
-        stat_summary(fun.y = mean, colour = "purple", geom = "point", shape = 18, size = 4, show_guide = FALSE) +
-        geom_text(data = means, aes(label = round(Recovery, 0), hjust = 2), size = 6) +
-        scale_fill_manual(values = c("darkorange", "lightblue")) +
-        labs(y = "Recovery (%)")
-dev.print(pdf, "boxplot.pdf", height = 4.5, width = 8)
+        geom_point(size = 4, show_guide = FALSE) +
+        #stat_summary(fun.y = mean, colour = "purple", geom = "point", shape = 18, size = 4, show_guide = FALSE) +
+        #geom_text(data = means, aes(label = round(Peak, 0), hjust = 2), size = 6) +
+        scale_fill_manual(values = c("darkorange", "lightblue", "lightgreen", "maroon")) +
+        labs(x = NULL, y = "Abundance (counts)")+
+        theme_bw(base_size = 20)+
+        theme(legend.position = "none")+
+        facet_grid(. ~ Sulfate_red)
+dev.print(pdf, "boxplot-all.pdf", height = 4.5, width = 8)
