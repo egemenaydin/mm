ee1bb86800dc33396a4345bb40eadd3a40496ad3
egemenaydin <egemenaydin1@gmail.com>
2015-06-29 14:22:45 -0500

    3 plots for all time points

--- van-kravelen.R
+++ van-kravelen.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 library(qdap)
+library(dplyr)
+library(gridExtra)
 df <- read.csv("formula.csv")
 
 df$Formula <- gsub("([0-9])([A-Z])", "\\1~\\2", df$Formula)
@@ -19,4 +21,40 @@ df[is.na(df)] <- 0
 df$OC <- df$O/df$C
 df$HC <- df$H/df$C
 
-ggplot(df, aes(df$HC, df$OC)) + geom_point()
+df$tp0 <- apply(select(df, FE1.tp0.a, FE1.tp0.b), 1, function(x){
+        log2(mean(x[1:2]))
+})
+
+df$tp1 <- apply(select(df, FE1.tp2.a, FE1.tp2.b), 1, function(x){
+        log2(mean(x[1:2]))
+})
+
+df$tp2 <- apply(select(df, FE1.tp4.a, FE1.tp4.b), 1, function(x){
+        log2(mean(x[1:2]))
+})
+
+p1 <- ggplot(df, aes(df$HC, df$OC, colour = tp0)) +
+        geom_point(size = 4) +
+        scale_color_gradient2() +
+        xlab("H/C") +
+        ylab("O/C") +
+        ggtitle("Camelina day 0") +
+        theme_bw()
+
+p2 <- ggplot(df, aes(df$HC, df$OC, colour = tp1)) +
+        geom_point(size = 4) +
+        scale_color_gradient2() +
+        xlab("H/C") +
+        ylab("O/C") +
+        ggtitle("Camelina day 20") +
+        theme_bw()
+
+p3 <- ggplot(df, aes(df$HC, df$OC, colour = tp2)) +
+        geom_point(size = 4) +
+        scale_color_gradient2() +
+        xlab("H/C") +
+        ylab("O/C") +
+        ggtitle("Camelina day 40") +
+        theme_bw()
+
+grid.arrange(p1, p2, p3)
