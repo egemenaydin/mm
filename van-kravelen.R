57812f334a15b19832e36d04912d71950d7bc4e8
Egemen Aydin <egemenaydin1@gmail.com>
2015-06-28 23:52:35 -0500

    initial commit

---
+++ van-kravelen.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(qdap)
+df <- read.csv("van-kravelen-trial.csv")
+
+df$Formula <- gsub("([0-9])([A-Z])", "\\1~\\2", df$Formula)
+df$Formula <- gsub("([A-Z])([A-Z])", "\\1~\\2", df$Formula)
+df$Formula <- gsub("([A-Z])([A-Z])", "\\1~\\2", df$Formula)
+df$Formula <- paste0(df$Formula, "~")
+df$Formula <- gsub("([A-Z])(~)", "\\11\\2", df$Formula)
+
+
+
+df$C <- as.numeric(genXtract(df$Formula, "C", "~"))
+df$H <- as.numeric(genXtract(df$Formula, "H", "~"))
+df$O <- as.numeric(genXtract(df$Formula, "O", "~"))
+
+df[is.na(df)] <- 0
+
+df$OC <- df$O/df$C
+df$HC <- df$H/df$C
+
+ggplot(df, aes(df$HC, df$OC)) + geom_point()
