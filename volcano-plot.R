c1b361325dad831ac450fe90ebbb3c0a020509c3
egemenaydin <egemenaydin1@gmail.com>
2015-06-30 12:24:29 -0500

    dplyr algorithm

--- volcano-plot.R
+++ volcano-plot.R
@@ -1,27 +1,25 @@
-data <- read.csv("PoE_217AandD_raw_nonaveraged.csv", check.names = FALSE)
+library(dplyr)
+data <- read.csv("base_in_feature_extract_vol.csv", check.names = FALSE)
 
 rownames(data) <- make.names(data[, 1], unique = TRUE)
-data$Compound <- NULL
+data$mz <- NULL
 data[is.na(data)] <- 1
 data[data == 0] <- 1
 
 data_polished <- data[rowSums(data) != ncol(data), ]
 data_polished[data_polished == 1] <- 0.5*(min(data_polished[data_polished>1],na.rm=TRUE))
 
-
-data.frame(data_polished, data_polished$p <- apply(data_polished, 1, function(x) {
+data_polished$p <- apply(select(data_polished, F1:S3), 1, function(x) {
         t.test(x[1:3], x[4:6], paired = TRUE)$p.value
-} ))
+} )
 
-data.frame(data_polished, data_polished$log_p <- apply(data_polished, 1, function(y){
-        -log10(y[7])
-}))
+data_polished$log_p <- -log10(data_polished$p)
 
-data.frame(data_polished, data_polished$dif <- apply(data_polished, 1, function(z){
+data_polished$dif <- apply(select(data_polished, F1:S3), 1, function(z) {
         log2(mean(z[4:6], na.rm = TRUE) / mean(z[1:3], na.rm = TRUE))
-}))
+})
 
-data_polished$decreased <- as.factor(data_polished$dif < -1 & data_polished$log_p > 2)
+data_polished$decreased <- as.factor(data_polished$dif < -1 & data_polished$log_p > 1.5)
 
 df <- data.frame(data_polished)
 
@@ -29,7 +27,7 @@ t1 <- subset(df, df$decreased == "TRUE")
 
 f1 <- subset(df, df$decreased == "FALSE")
 
-df$increased <- as.factor(data_polished$dif > 1 & data_polished$log_p > 2)
+df$increased <- as.factor(data_polished$dif > 1 & data_polished$log_p > 1.5)
 
 t2 <- subset(df, df$increased == "TRUE")
 
@@ -37,7 +35,7 @@ f2 <- subset(df, df$increased == "FALSE")
 
 all_false <- subset(df, df$increased == "FALSE" & df$decreased == "FALSE")
 
-par(xpd = TRUE, mar = par()$mar + c(0, 5, 0, 0))
+#par(xpd = TRUE, mar = par()$mar + c(0, 5, 0, 0))
 
 with(df, plot(dif, log_p, xlab = "log2 Fold Change", ylab = "-log10(P)", pch = 20, col = "skyblue", cex = 0.5))
 
@@ -45,11 +43,11 @@ with(t1, points(dif, log_p, col = "maroon", pch = 20, cex = 0.5))
 
 with(t2, points(dif, log_p, col = "golden rod", pch = 20, cex = 0.5))
 
-legend(-38, 6, xpd= TRUE,  pch = 20, col = c("maroon", "golden rod"), legend = c("Decreased", "Increased"), bty = "n")
+#legend(-38, 6, xpd= TRUE,  pch = 20, col = c("maroon", "golden rod"), legend = c("Decreased", "Increased"), bty = "n")
 
-dev.print(pdf, "217AandD.pdf", height=5, width=8.5)
+dev.print(pdf, "Camelina-SSW.pdf", height=5, width=8.5)
 
-write.csv(t1, "217AandD_decreased.csv")
+write.csv(t1, "Camelina.csv")
 
 write.csv(t2, "217AandD_increased.csv")
 
