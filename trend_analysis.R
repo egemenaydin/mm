5640b54e29ebe9a0031fc58af6a25528b1bd22a4
egemenaydin <egemenaydin1@gmail.com>
2015-07-03 16:11:18 -0500

    different modes added

--- trend_analysis.R
+++ trend_analysis.R
@@ -4,10 +4,10 @@ library(RColorBrewer)
 
 #RSW Camelina
 
-df <- read.csv("positive_base_int.csv")
+df <- read.csv("base_in_feature_extract.csv")
 df <- select(df, mz, rt, FE4.tp0.a:FE20.tp4.b)
 
-df <- baseC_long
+#df <- baseC_long
 
 data.frame(df, df$Camelina.tp0 <- apply(select(df, FE5.tp0.a, FE5.tp0.b), 1, mean))
 data.frame(df, df$Camelina.tp1 <- apply(select(df, FE5.tp2.a, FE5.tp2.b), 1, mean))
@@ -37,7 +37,7 @@ data.frame(df, df$No.fuel.abiotic.tp0.sd <- apply(select(df, FE19.tp0.a, FE19.tp
 data.frame(df, df$No.fuel.abiotic.tp1.sd <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, sd))
 data.frame(df, df$No.fuel.abiotic.tp2.sd <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, sd))
 
-df_decrease <- filter(df, Camelina.tp0 / Camelina.tp1 > 3 | Camelina.tp0 / Camelina.tp2 > 3 & Camelina.tp0 / No.fuel.tp2 >3)
+df_decrease <- filter(df, Camelina.tp0 / Camelina.tp1 > 3 | Camelina.tp0 / Camelina.tp2 > 3 & Camelina.tp0 / No.fuel.tp0 >3)
 df_increase <- filter(df, Camelina.tp1 / Camelina.tp0 > 3 | Camelina.tp2 / Camelina.tp0 > 3 & Camelina.tp2 / No.fuel.tp2 >3)
 
 write.csv(df_decrease, "decrease.csv")
@@ -107,40 +107,40 @@ for (i in 1:x_ic){
 library(dplyr)
 library(ggplot2)
 library(RColorBrewer)
-df <- read.csv("positive_base_int.csv")
-df <- select(df, mz, rt, FE4.tp0.a:FE20.tp4.b)
-df <- baseC_long
+df <- read.csv("base_in_feature_extract.csv")
+df <- select(df, mz, rt, FE14.tp0.a:FE20.tp4.b)
+#df <- baseC_long
 
-data.frame(df, df$Camelina.tp0 <- apply(select(df, FE15.tp0.a, FE15.tp0.b), 1, mean))
-data.frame(df, df$Camelina.tp1 <- apply(select(df, FE15.tp2.a, FE15.tp2.b), 1, mean))
-data.frame(df, df$Camelina.tp2 <- apply(select(df, FE15.tp4.a, FE15.tp4.b), 1, mean))
+data.frame(df, df$F76.tp0 <- apply(select(df, FE15.tp0.a, FE15.tp0.b), 1, mean))
+data.frame(df, df$F76.tp1 <- apply(select(df, FE15.tp2.a, FE15.tp2.b), 1, mean))
+data.frame(df, df$F76.tp2 <- apply(select(df, FE15.tp4.a, FE15.tp4.b), 1, mean))
 data.frame(df, df$No.fuel.tp0 <- apply(select(df, FE20.tp0.a, FE20.tp0.b), 1, mean))
 data.frame(df, df$No.fuel.tp1 <- apply(select(df, FE20.tp2.a, FE20.tp2.b), 1, mean))
 data.frame(df, df$No.fuel.tp2 <- apply(select(df, FE20.tp4.a, FE20.tp4.b), 1, mean))
 
-data.frame(df, df$Camelina.abiotic.tp0 <- apply(select(df, FE14.tp0.a, FE14.tp0.b), 1, mean))
-data.frame(df, df$Camelina.abiotic.tp1 <- apply(select(df, FE14.tp2.a, FE14.tp2.b), 1, mean))
-data.frame(df, df$Camelina.abiotic.tp2 <- apply(select(df, FE14.tp4.a, FE14.tp4.b), 1, mean))
+data.frame(df, df$F76.abiotic.tp0 <- apply(select(df, FE14.tp0.a, FE14.tp0.b), 1, mean))
+data.frame(df, df$F76.abiotic.tp1 <- apply(select(df, FE14.tp2.a, FE14.tp2.b), 1, mean))
+data.frame(df, df$F76.abiotic.tp2 <- apply(select(df, FE14.tp4.a, FE14.tp4.b), 1, mean))
 data.frame(df, df$No.fuel.abiotic.tp0 <- apply(select(df, FE19.tp0.a, FE19.tp0.b), 1, mean))
 data.frame(df, df$No.fuel.abiotic.tp1 <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, mean))
 data.frame(df, df$No.fuel.abiotic.tp2 <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, mean))
 
-data.frame(df, df$Camelina.tp0.sd <- apply(select(df, FE15.tp0.a, FE15.tp0.b), 1, sd))
-data.frame(df, df$Camelina.tp1.sd <- apply(select(df, FE15.tp2.a, FE15.tp2.b), 1, sd))
-data.frame(df, df$Camelina.tp2.sd <- apply(select(df, FE15.tp4.a, FE15.tp4.b), 1, sd))
+data.frame(df, df$F76.tp0.sd <- apply(select(df, FE15.tp0.a, FE15.tp0.b), 1, sd))
+data.frame(df, df$F76.tp1.sd <- apply(select(df, FE15.tp2.a, FE15.tp2.b), 1, sd))
+data.frame(df, df$F76.tp2.sd <- apply(select(df, FE15.tp4.a, FE15.tp4.b), 1, sd))
 data.frame(df, df$No.fuel.tp0.sd <- apply(select(df, FE20.tp0.a, FE20.tp0.b), 1, sd))
 data.frame(df, df$No.fuel.tp1.sd <- apply(select(df, FE20.tp2.a, FE20.tp2.b), 1, sd))
 data.frame(df, df$No.fuel.tp2.sd <- apply(select(df, FE20.tp4.a, FE20.tp4.b), 1, sd))
 
-data.frame(df, df$Camelina.abiotic.tp0.sd <- apply(select(df, FE14.tp0.a, FE14.tp0.b), 1, sd))
-data.frame(df, df$Camelina.abiotic.tp1.sd <- apply(select(df, FE14.tp2.a, FE14.tp2.b), 1, sd))
-data.frame(df, df$Camelina.abiotic.tp2.sd <- apply(select(df, FE14.tp4.a, FE14.tp4.b), 1, sd))
+data.frame(df, df$F76.abiotic.tp0.sd <- apply(select(df, FE14.tp0.a, FE14.tp0.b), 1, sd))
+data.frame(df, df$F76.abiotic.tp1.sd <- apply(select(df, FE14.tp2.a, FE14.tp2.b), 1, sd))
+data.frame(df, df$F76.abiotic.tp2.sd <- apply(select(df, FE14.tp4.a, FE14.tp4.b), 1, sd))
 data.frame(df, df$No.fuel.abiotic.tp0.sd <- apply(select(df, FE19.tp0.a, FE19.tp0.b), 1, sd))
 data.frame(df, df$No.fuel.abiotic.tp1.sd <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, sd))
 data.frame(df, df$No.fuel.abiotic.tp2.sd <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, sd))
 
-df_decrease <- filter(df, Camelina.tp0 / Camelina.tp1 > 3 | Camel/home/egemen/Desktop/Oklahoma/fuel-extract/degradation/RSW-camelina/volcano/base_in_feature_extract.csvina.tp0 / Camelina.tp2 > 3 & Camelina.tp0 / No.fuel.tp2 >3)
-df_increase <- filter(df, Camelina.tp1 / Camelina.tp0 > 3 | Camelina.tp2 / Camelina.tp0 > 3 & Camelina.tp2 / No.fuel.tp2 >3)
+df_decrease <- filter(df, F76.tp0 / F76.tp1 > 3 | F76.tp0 / F76.tp2 > 3 & F76.tp0 / No.fuel.tp0 >3)
+df_increase <- filter(df, F76.tp1 / F76.tp0 > 3 | F76.tp2 / F76.tp0 > 3 & F76.tp2 / No.fuel.tp2 >3)
 
 write.csv(df_decrease, "decrease.csv")
 write.csv(df_increase, "increase.csv")
@@ -151,10 +151,10 @@ df2 <- rbind(df_decrease, df_increase)
 write.csv(df2, "increase_decrease.csv")
 
 p <- ggplot(df2, aes(x = mz))
-p +  geom_histogram(binwidth = 10)+ facet_grid(trend~.) + theme_bw(base_size = 18)
+p +  geom_histogram(binwidth = 10)+ facet_grid(trend~.) + theme_bw(base_size = 22)
 
 
-dev.print(pdf, "camelina_increase_decrease_hist.pdf", height = 5, width = 10)
+dev.print(pdf, "F76_increase_decrease_hist.pdf", height = 5, width = 10)
 
 inc <- read.csv("increase2.csv", check.names = FALSE)
 dec <- read.csv("decrease2.csv", check.names = FALSE)
@@ -451,6 +451,104 @@ p +  geom_histogram(binwidth = 10)+ facet_grid(trend~.) + theme_bw(base_size = 1
 dev.print(pdf, "camelina_increase_decrease_hist.pdf", height = 5, width = 10)
 
 inc <- read.csv("increase2.csv", check.names = FALSE)
+dec <- read.csv("Camelina-rsw-change.csv", check.names = FALSE)
+
+setwd("~/mm/decrease/")
+
+tit <- names(dec)
+tit <- tit[(-c(1, 2))]
+
+m <- reshape2::melt(dec, id = c("Fuel", "time"))
+p <- ggplot(m, aes(x = time, y = value, color = Fuel, group = Fuel)) +
+        geom_point(size = 4) + 
+        scale_color_brewer(type = "qual", palette = "Set1") +
+        xlab("Time (days)") +
+        ylab("Intensity (counts)") +
+        theme_bw(base_size =16)
+p1 <- plyr::dlply(m, "variable", '%+%', e1 = p)
+
+x <- length(p1)
+
+for (i in 1:x){
+        png(file = paste(tit[[i]], ".png", sep = ""))
+        print(p1[[i]])
+        dev.off()
+}
+
+setwd("~/GitHub/mm/increase/")
+
+tit_ic <- names(inc)
+tit_ic <- tit_ic[(-c(1, 2))]
+
+ic <- reshape2::melt(inc, id = c("Fuel", "time"))
+p_ic <- ggplot(ic, aes(x = time, y = value, color = Fuel, group = Fuel)) +
+        geom_point(size = 4) + 
+        scale_color_brewer(type = "qual", palette = "Set1") +
+        xlab("Time (days)") +
+        ylab("Intensity (counts)") +
+        theme_bw(base_size =16)
+p1_ic <- plyr::dlply(ic, "variable", '%+%', e1 = p_ic)
+
+x_ic <- length(p1_ic)
+
+for (i in 1:x_ic){
+        png(file = paste(tit_ic[[i]], ".png", sep = ""))
+        print(p1_ic[[i]])
+        dev.off()
+}
+
+
+#sterile seawater Camelina
+df <- read.csv("base_in_feature_extract.csv")
+df <- select(df, mz, rt, FE4.tp0.a:FE18.tp4.b)
+#df <- baseC_long
+
+data.frame(df, df$Camelina.tp0 <- apply(select(df, FE3.tp0.a, FE3.tp0.b), 1, mean))
+data.frame(df, df$Camelina.tp1 <- apply(select(df, FE3.tp2.a, FE3.tp2.b), 1, mean))
+data.frame(df, df$Camelina.tp2 <- apply(select(df, FE3.tp4.a, FE3.tp4.b), 1, mean))
+data.frame(df, df$No.fuel.tp0 <- apply(select(df, FE18.tp0.a, FE18.tp0.b), 1, mean))
+data.frame(df, df$No.fuel.tp1 <- apply(select(df, FE18.tp2.a, FE18.tp2.b), 1, mean))
+data.frame(df, df$No.fuel.tp2 <- apply(select(df, FE18.tp4.a, FE18.tp4.b), 1, mean))
+
+data.frame(df, df$Camelina.abiotic.tp0 <- apply(select(df, FE4.tp0.a, FE4.tp0.b), 1, mean))
+data.frame(df, df$Camelina.abiotic.tp1 <- apply(select(df, FE4.tp2.a, FE4.tp2.b), 1, mean))
+data.frame(df, df$Camelina.abiotic.tp2 <- apply(select(df, FE4.tp4.a, FE4.tp4.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp0 <- apply(select(df, FE19.tp0.a, FE19.tp0.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp1 <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp2 <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, mean))
+
+data.frame(df, df$Camelina.tp0.sd <- apply(select(df, FE3.tp0.a, FE3.tp0.b), 1, sd))
+data.frame(df, df$Camelina.tp1.sd <- apply(select(df, FE3.tp2.a, FE3.tp2.b), 1, sd))
+data.frame(df, df$Camelina.tp2.sd <- apply(select(df, FE3.tp4.a, FE3.tp4.b), 1, sd))
+data.frame(df, df$No.fuel.tp0.sd <- apply(select(df, FE18.tp0.a, FE18.tp0.b), 1, sd))
+data.frame(df, df$No.fuel.tp1.sd <- apply(select(df, FE18.tp2.a, FE18.tp2.b), 1, sd))
+data.frame(df, df$No.fuel.tp2.sd <- apply(select(df, FE18.tp4.a, FE18.tp4.b), 1, sd))
+
+data.frame(df, df$Camelina.abiotic.tp0.sd <- apply(select(df, FE4.tp0.a, FE4.tp0.b), 1, sd))
+data.frame(df, df$Camelina.abiotic.tp1.sd <- apply(select(df, FE4.tp2.a, FE4.tp2.b), 1, sd))
+data.frame(df, df$Camelina.abiotic.tp2.sd <- apply(select(df, FE4.tp4.a, FE4.tp4.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp0.sd <- apply(select(df, FE19.tp0.a, FE19.tp0.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp1.sd <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp2.sd <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, sd))
+
+df_decrease <- filter(df, Camelina.tp0 / Camelina.tp1 > 3 | Camelina.tp0 / Camelina.tp2 > 3 & Camelina.tp0 / No.fuel.tp0 >3)
+df_increase <- filter(df, Camelina.tp1 / Camelina.tp0 > 3 | Camelina.tp2 / Camelina.tp0 > 3 & Camelina.tp2 / No.fuel.tp2 >3)
+
+write.csv(df_decrease, "decrease.csv")
+write.csv(df_increase, "increase.csv")
+
+df_decrease$trend <- "decrease"
+df_increase$trend <- "increase"
+df2 <- rbind(df_decrease, df_increase)
+write.csv(df2, "increase_decrease.csv")
+
+p <- ggplot(df2, aes(x = mz))
+p +  geom_histogram(binwidth = 10)+ facet_grid(trend~.) + theme_bw(base_size = 22) + xlab("Mass") + ylab("Number of compounds")
+
+
+dev.print(pdf, "Camelina_increase_decrease_hist.pdf", height = 5, width = 10)
+
+inc <- read.csv("increase2.csv", check.names = FALSE)
 dec <- read.csv("decrease2.csv", check.names = FALSE)
 
 setwd("~/GitHub/mm/decrease/")
@@ -499,9 +597,197 @@ for (i in 1:x_ic){
 
 
 
+#sterile seawater F76
+df <- read.csv("base_in_feature_extract.csv")
+df <- select(df, mz, rt, FE14.tp0.a:FE18.tp4.b)
+#df <- baseC_long
+
+data.frame(df, df$F76.tp0 <- apply(select(df, FE13.tp0.a, FE13.tp0.b), 1, mean))
+data.frame(df, df$F76.tp1 <- apply(select(df, FE13.tp2.a, FE13.tp2.b), 1, mean))
+data.frame(df, df$F76.tp2 <- apply(select(df, FE13.tp4.a, FE13.tp4.b), 1, mean))
+data.frame(df, df$No.fuel.tp0 <- apply(select(df, FE18.tp0.a, FE18.tp0.b), 1, mean))
+data.frame(df, df$No.fuel.tp1 <- apply(select(df, FE18.tp2.a, FE18.tp2.b), 1, mean))
+data.frame(df, df$No.fuel.tp2 <- apply(select(df, FE18.tp4.a, FE18.tp4.b), 1, mean))
+
+data.frame(df, df$F76.abiotic.tp0 <- apply(select(df, FE14.tp0.a, FE14.tp0.b), 1, mean))
+data.frame(df, df$F76.abiotic.tp1 <- apply(select(df, FE14.tp2.a, FE14.tp2.b), 1, mean))
+data.frame(df, df$F76.abiotic.tp2 <- apply(select(df, FE14.tp4.a, FE14.tp4.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp0 <- apply(select(df, FE19.tp0.a, FE19.tp0.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp1 <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp2 <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, mean))
+
+data.frame(df, df$F76.tp0.sd <- apply(select(df, FE13.tp0.a, FE13.tp0.b), 1, sd))
+data.frame(df, df$F76.tp1.sd <- apply(select(df, FE13.tp2.a, FE13.tp2.b), 1, sd))
+data.frame(df, df$F76.tp2.sd <- apply(select(df, FE13.tp4.a, FE13.tp4.b), 1, sd))
+data.frame(df, df$No.fuel.tp0.sd <- apply(select(df, FE18.tp0.a, FE18.tp0.b), 1, sd))
+data.frame(df, df$No.fuel.tp1.sd <- apply(select(df, FE18.tp2.a, FE18.tp2.b), 1, sd))
+data.frame(df, df$No.fuel.tp2.sd <- apply(select(df, FE18.tp4.a, FE18.tp4.b), 1, sd))
+
+data.frame(df, df$F76.abiotic.tp0.sd <- apply(select(df, FE14.tp0.a, FE14.tp0.b), 1, sd))
+data.frame(df, df$F76.abiotic.tp1.sd <- apply(select(df, FE14.tp2.a, FE14.tp2.b), 1, sd))
+data.frame(df, df$F76.abiotic.tp2.sd <- apply(select(df, FE14.tp4.a, FE14.tp4.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp0.sd <- apply(select(df, FE19.tp0.a, FE19.tp0.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp1.sd <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp2.sd <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, sd))
+
+df_decrease <- filter(df, F76.tp0 / F76.tp1 > 3 | F76.tp0 / F76.tp2 > 3 & F76.tp0 / No.fuel.tp0 >3)
+df_increase <- filter(df, F76.tp1 / F76.tp0 > 3 | F76.tp2 / F76.tp0 > 3 & F76.tp2 / No.fuel.tp2 >3)
+
+write.csv(df_decrease, "decrease.csv")
+write.csv(df_increase, "increase.csv")
+
+df_decrease$trend <- "decrease"
+df_increase$trend <- "increase"
+df2 <- rbind(df_decrease, df_increase)
+write.csv(df2, "increase_decrease.csv")
+
+p <- ggplot(df2, aes(x = mz))
+p +  geom_histogram(binwidth = 10)+ facet_grid(trend~.) + theme_bw(base_size = 22) + xlab("Mass") + ylab("Number of compounds")
 
 
+dev.print(pdf, "F76_increase_decrease_hist.pdf", height = 5, width = 10)
+
+inc <- read.csv("increase2.csv", check.names = FALSE)
+dec <- read.csv("decrease2.csv", check.names = FALSE)
+
+setwd("~/GitHub/mm/decrease/")
+
+tit <- names(dec)
+tit <- tit[(-c(1, 2))]
+
+m <- reshape2::melt(dec, id = c("Fuel", "time"))
+p <- ggplot(m, aes(x = time, y = value, color = Fuel, group = Fuel)) +
+        geom_point(size = 4) + 
+        scale_color_brewer(type = "qual", palette = "Set1") +
+        xlab("Time (days)") +
+        ylab("Intensity (counts)") +
+        theme_bw(base_size =16)
+p1 <- plyr::dlply(m, "variable", '%+%', e1 = p)
+
+x <- length(p1)
+
+for (i in 1:x){
+        png(file = paste(tit[[i]], ".png", sep = ""))
+        print(p1[[i]])
+        dev.off()
+}
+
+setwd("~/GitHub/mm/increase/")
+
+tit_ic <- names(inc)
+tit_ic <- tit_ic[(-c(1, 2))]
+
+ic <- reshape2::melt(inc, id = c("Fuel", "time"))
+p_ic <- ggplot(ic, aes(x = time, y = value, color = Fuel, group = Fuel)) +
+        geom_point(size = 4) + 
+        scale_color_brewer(type = "qual", palette = "Set1") +
+        xlab("Time (days)") +
+        ylab("Intensity (counts)") +
+        theme_bw(base_size =16)
+p1_ic <- plyr::dlply(ic, "variable", '%+%', e1 = p_ic)
+
+x_ic <- length(p1_ic)
+
+for (i in 1:x_ic){
+        png(file = paste(tit_ic[[i]], ".png", sep = ""))
+        print(p1_ic[[i]])
+        dev.off()
+}
 
 
+#sterile seawater FTF76
+df <- read.csv("base_in_feature_extract.csv")
+df <- select(df, mz, rt, FE9.tp0.a:FE18.tp4.b)
+#df <- baseC_long
+
+data.frame(df, df$FTF76.tp0 <- apply(select(df, FE8.tp0.a, FE8.tp0.b), 1, mean))
+data.frame(df, df$FTF76.tp1 <- apply(select(df, FE8.tp2.a, FE8.tp2.b), 1, mean))
+data.frame(df, df$FTF76.tp2 <- apply(select(df, FE8.tp4.a, FE8.tp4.b), 1, mean))
+data.frame(df, df$No.fuel.tp0 <- apply(select(df, FE18.tp0.a, FE18.tp0.b), 1, mean))
+data.frame(df, df$No.fuel.tp1 <- apply(select(df, FE18.tp2.a, FE18.tp2.b), 1, mean))
+data.frame(df, df$No.fuel.tp2 <- apply(select(df, FE18.tp4.a, FE18.tp4.b), 1, mean))
+
+data.frame(df, df$FTF76.abiotic.tp0 <- apply(select(df, FE9.tp0.a, FE9.tp0.b), 1, mean))
+data.frame(df, df$FTF76.abiotic.tp1 <- apply(select(df, FE9.tp2.a, FE9.tp2.b), 1, mean))
+data.frame(df, df$FTF76.abiotic.tp2 <- apply(select(df, FE9.tp4.a, FE9.tp4.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp0 <- apply(select(df, FE19.tp0.a, FE19.tp0.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp1 <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, mean))
+data.frame(df, df$No.fuel.abiotic.tp2 <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, mean))
+
+data.frame(df, df$FTF76.tp0.sd <- apply(select(df, FE8.tp0.a, FE8.tp0.b), 1, sd))
+data.frame(df, df$FTF76.tp1.sd <- apply(select(df, FE8.tp2.a, FE8.tp2.b), 1, sd))
+data.frame(df, df$FTF76.tp2.sd <- apply(select(df, FE8.tp4.a, FE8.tp4.b), 1, sd))
+data.frame(df, df$No.fuel.tp0.sd <- apply(select(df, FE18.tp0.a, FE18.tp0.b), 1, sd))
+data.frame(df, df$No.fuel.tp1.sd <- apply(select(df, FE18.tp2.a, FE18.tp2.b), 1, sd))
+data.frame(df, df$No.fuel.tp2.sd <- apply(select(df, FE18.tp4.a, FE18.tp4.b), 1, sd))
+
+data.frame(df, df$FTF76.abiotic.tp0.sd <- apply(select(df, FE9.tp0.a, FE9.tp0.b), 1, sd))
+data.frame(df, df$FTF76.abiotic.tp1.sd <- apply(select(df, FE9.tp2.a, FE9.tp2.b), 1, sd))
+data.frame(df, df$FTF76.abiotic.tp2.sd <- apply(select(df, FE9.tp4.a, FE9.tp4.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp0.sd <- apply(select(df, FE19.tp0.a, FE19.tp0.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp1.sd <- apply(select(df, FE19.tp2.a, FE19.tp2.b), 1, sd))
+data.frame(df, df$No.fuel.abiotic.tp2.sd <- apply(select(df, FE19.tp4.a, FE19.tp4.b), 1, sd))
+
+df_decrease <- filter(df, FTF76.tp0 / FTF76.tp1 > 3 | FTF76.tp0 / FTF76.tp2 > 3 & FTF76.tp0 / No.fuel.tp0 >3)
+df_increase <- filter(df, FTF76.tp1 / FTF76.tp0 > 3 | FTF76.tp2 / FTF76.tp0 > 3 & FTF76.tp2 / No.fuel.tp2 >3)
+
+write.csv(df_decrease, "decrease.csv")
+write.csv(df_increase, "increase.csv")
+
+df_decrease$trend <- "decrease"
+df_increase$trend <- "increase"
+df2 <- rbind(df_decrease, df_increase)
+write.csv(df2, "increase_decrease.csv")
+
+p <- ggplot(df2, aes(x = mz))
+p +  geom_histogram(binwidth = 10)+ facet_grid(trend~.) + theme_bw(base_size = 22) + xlab("Mass") + ylab("Number of compounds")
 
 
+dev.print(pdf, "FTF76_increase_decrease_hist.pdf", height = 5, width = 10)
+
+inc <- read.csv("increase2.csv", check.names = FALSE)
+dec <- read.csv("decrease2.csv", check.names = FALSE)
+
+setwd("~/GitHub/mm/decrease/")
+
+tit <- names(dec)
+tit <- tit[(-c(1, 2))]
+
+m <- reshape2::melt(dec, id = c("Fuel", "time"))
+p <- ggplot(m, aes(x = time, y = value, color = Fuel, group = Fuel)) +
+        geom_point(size = 4) + 
+        scale_color_brewer(type = "qual", palette = "Set1") +
+        xlab("Time (days)") +
+        ylab("Intensity (counts)") +
+        theme_bw(base_size =16)
+p1 <- plyr::dlply(m, "variable", '%+%', e1 = p)
+
+x <- length(p1)
+
+for (i in 1:x){
+        png(file = paste(tit[[i]], ".png", sep = ""))
+        print(p1[[i]])
+        dev.off()
+}
+
+setwd("~/GitHub/mm/increase/")
+
+tit_ic <- names(inc)
+tit_ic <- tit_ic[(-c(1, 2))]
+
+ic <- reshape2::melt(inc, id = c("Fuel", "time"))
+p_ic <- ggplot(ic, aes(x = time, y = value, color = Fuel, group = Fuel)) +
+        geom_point(size = 4) + 
+        scale_color_brewer(type = "qual", palette = "Set1") +
+        xlab("Time (days)") +
+        ylab("Intensity (counts)") +
+        theme_bw(base_size =16)
+p1_ic <- plyr::dlply(ic, "variable", '%+%', e1 = p_ic)
+
+x_ic <- length(p1_ic)
+
+for (i in 1:x_ic){
+        png(file = paste(tit_ic[[i]], ".png", sep = ""))
+        print(p1_ic[[i]])
+        dev.off()
+}
