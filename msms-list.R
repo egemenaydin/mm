da4cd2f27a4b43b7a5f0d3d0b4e4a151ef3c0c66
egemenaydin <egemenaydin1@gmail.com>
2015-07-28 12:47:48 -0500

    negative and positive ionization total lists lists for plots

--- msms-list.R
+++ msms-list.R
@@ -1,8 +1,38 @@
 library(dplyr)
-data <- read.csv("positive_featurelist.csv")
-iso.data <- filter(data, isotopes != "")
-base <- filter(iso.data, grepl("\\[M\\]\\+", isotopes))
-write.csv(base, "base_in_feature_extract.csv")
+
+#positive ionization mode
+dataP <- read.csv("positive_featurelist.csv")
+dataP$ionization <- "positive"
+iso.dataP <- filter(dataP, isotopes != "")
+baseP <- filter(iso.dataP, grepl("\\[M\\]\\+", isotopes))
+write.csv(baseP, "positive_base_in_feature_extract.csv")
+
+#negative ionization mode
+dataN <- read.csv("negative_featurelist.csv")
+dataN$ionization <- "negative"
+iso.dataN <- filter(dataN, isotopes != "")
+baseN <- filter(iso.dataN, grepl("\\[M\\]\\-", isotopes))
+write.csv(baseN, "positive_base_in_feature_extract.csv")
+
+#merge lists
+dataAll <- bind_rows(dataP, dataN)
+write.csv(dataAll, "all_features.csv")
+baseAll <- bind_rows(baseP, baseN)
+write.csv(baseAll, "base_peaks.csv")
+tAll <- data.frame(t(dataAll))
+write.csv(tAll, "transpose_all_features.csv")
+tBase <- data.frame(t(baseAll))
+write.csv(tBase, "transpose_base_peaks.csv")
+
+#lists for plots
+dataAll_PCA <- select(dataAll, MW1a:MW8c)
+dataAll_PCA <- data.frame(t(dataAll_PCA))
+baseAll_PCA <- select(baseAll, MW1a:MW8c)
+baseAll_PCA <- data.frame(t(baseAll_PCA))
+Samples <- c("MW1", "MW1", "MW1", "MW2", "MW2", "MW2", "MW3", "MW3", "MW3", "MW4", "MW4", "MW4", "MW5", "MW5", "MW5", "MW6", "MW6", "MW6", "MW7", "MW7", "MW7", "MW8", "MW8", "MW8")
+
+
+#MSMS list creation
 base$log.Art.SW <- log2(base$Art.SW)
 base$log.FE1.2 <- log2(base$FE1.2)
 base$fold_change <- base$log.FE1.2 / base$log.Art.SW
