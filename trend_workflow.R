6fd1107ff48880d6baa029a6a542d36f943ef76a
egemenaydin <egemenaydin1@gmail.com>
2015-07-31 10:33:32 -0500

    shorten the code with better assignments added media restrain to filter

--- trend_workflow.R
+++ trend_workflow.R
@@ -33,36 +33,36 @@ df$p3 <- apply(select(df, HB1a:HB3b, Blank_004:Blank_002), 1, function(x) {
 } )
 df$logp3 <- log(df$p1, base = 10)
 
-df <- filter(df, NoBiocide > LC & NoBiocide > HC & NoBiocide > Blank)
-df <- filter(df, logp1 < -2 | logp2 < -2)
-Csub <- select(df, name:Blank_002)
-row.names(Csub) <- Csub$name
-Csub$name <- NULL
+df.D <- filter(df, NoBiocide > LC & NoBiocide > HC & NoBiocide > Blank & NoBiocide > 10*media)
+df.D <- filter(df.D, logp1 < -2 | logp2 < -2)
+Csub.D <- select(df.D, name:Blank_002)
+row.names(Csub.D) <- Csub.D$name
+Csub.D$name <- NULL
 
 ##transpose
-Csub <- data.frame(t(Csub))
+Csub.D <- data.frame(t(Csub.D))
 sample <- c("media", "media", "media", "media", "media", "media", "No Biocide", "No Biocide", "No Biocide", "No Biocide", "No Biocide", "No Biocide", "Low Concentration", "Low Concentration", "Low Concentration", "Low Concentration", "Low Concentration", "Low Concentration", "High Concentration", "High Concentration", "High Concentration", "High Concentration", "High Concentration", "High Concentration", "Pooled", "Pooled", "Pooled", "Blank", "Blank", "Blank")
-Csub$sample <- sample
+Csub.D$sample <- sample
 
 #tidy more
-m <- reshape2::melt(Csub, id = c("sample"))
-m$sample1 <- factor(m$sample, as.character(m$sample))
+m.D <- reshape2::melt(Csub.D, id = c("sample"))
+m.D$sample1 <- factor(m.D$sample, as.character(m.D$sample))
 
 #boxplot
-p1 <- ggplot(m, aes(sample1, value)) +
+p1 <- ggplot(m.D, aes(sample1, value)) +
         geom_boxplot() +
         #scale_x_discrete(lables = c("media", "No Biocide", "Low Concentration", "High Concentration", "Blank"))
         xlab("Samples") +
         ylab("Intensity (counts)") +
-        ggtitle(m$variable) +
+        ggtitle(m.D$variable) +
         theme_bw(base_size =16) +
         theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-p2 <- plyr::dlply(m, "variable", '%+%', e1 = p1)
+p2 <- plyr::dlply(m.D, "variable", '%+%', e1 = p1)
 
-setwd("~/GitHub/mm/box2/")
+setwd("~/mm/down/")
 
-tit <- df$name
+tit <- df.D$name
 
 x <- length(p2)
 
@@ -76,37 +76,10 @@ colnames(DR) <- "name"
 write.csv(DR, "downregulated.csv")
 
 ##upregulated
-#tidy data
-##select base peaks
-df2 <- filter(df.m, isotopes != "")
-df2 <- filter(df2, grepl("\\[M\\]\\+", isotopes))
 
-##subset peak names and samples
-df2 <- select(df2, name, M1a:Blank_002)
-row.names(df2) <- df2$name
-df2$media <- apply(select(df2, M1a:M3b), 1, mean)
-df2$NoBiocide <- apply(select(df2, contains("HB")), 1, mean)
-df2$LC <- apply(select(df2, contains("Low")), 1, mean)
-df2$HC <- apply(select(df2, contains("High")), 1, mean)
-df2$Blank <- apply(select(df2, contains("Blank")), 1, mean)
-df2$p1 <- apply(select(df2, HB1a:Low3b), 1, function(x) {
-        t.test(x[1:6], x[7:12], paired = FALSE)$p.value
-} )
-df2$logp1 <- log(df2$p1, base = 10)
-
-df2$p2 <- apply(select(df2, HB1a:HB3b, High1a:High3b), 1, function(x) {
-        t.test(x[1:6], x[7:12], paired = FALSE)$p.value
-} )
-
-df2$logp2 <- log(df2$p1, base = 10)
-
-df2$p3 <- apply(select(df2, HB1a:HB3b, Blank_004:Blank_002), 1, function(x) {
-        t.test(x[1:6], x[7:12], paired = FALSE)$p.value
-} )
-df2$logp3 <- log(df2$p1, base = 10)
-df2 <- filter(df2, NoBiocide < LC | NoBiocide < HC)
-df2 <- filter(df2, logp1 < -2 | logp2 < -2)
-Csub2 <- select(df2, name:Blank_002)
+df.U <- filter(df, NoBiocide < LC | NoBiocide < HC & LC > 10*media)
+df.U <- filter(df.U, logp1 < -2 | logp2 < -2)
+Csub2 <- select(df.U, name:Blank_002)
 row.names(Csub2) <- Csub2$name
 Csub2$name <- NULL
 
@@ -131,9 +104,9 @@ p3 <- ggplot(m2, aes(sample1, value)) +
 
 p4 <- plyr::dlply(m2, "variable", '%+%', e1 = p3)
 
-setwd("~/GitHub/mm/box1/")
+setwd("~/mm/up/")
 
-tit2 <- df2$name
+tit2 <- df.U$name
 
 x2 <- length(p4)
 
