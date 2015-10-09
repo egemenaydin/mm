0159a83a933f9e8831234553dee98043362de62d
egemenaydin <egemenaydin1@gmail.com>
2015-10-09 16:51:18 -0500

    initial commit

---
+++ featurelist-rdisop.R
@@ -0,0 +1,28 @@
+library(dplyr)
+library(ggplot2)
+
+#load data
+df.m <- read.csv("positive_featurelist2.csv")
+
+#create names
+if("name" %in% colnames(df.m)){
+        cat("names were defined\n")
+} else{
+        df.m$name <- paste("M", round(df.m$mz, 3), "T", round(df.m$rt, 3), sep = "")        
+}
+
+
+#read phenodata
+PhD <- data.frame(read.csv("PhDP.csv"))
+fNames <- as.vector(PhD$X)
+fNames <- gsub("-", "\\.", fNames)
+fSamples <- as.vector(PhD$class)
+fSamples <- gsub("-", "\\.", fSamples)
+grs <- unique(fSamples)
+
+#tidy data
+##select base peaks
+df <- filter(df.m, grepl("\\[M\\]\\+", isotopes))
+df$iso.no <- grep("\\[[0123456789]+\\]", df$isotopes, value = FALSE)
+f <- read.csv("formula.csv")
+df.b <- merge(f,df, by = "iso.no")
