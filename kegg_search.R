4906ca9c5635de6df2fdc926c4c23fe9b60f0248
egemenaydin <egemenaydin1@gmail.com>
2016-01-12 16:34:43 +0200

    globalized: no need to customize for different datasets.

--- kegg_search.R
+++ kegg_search.R
@@ -1,7 +1,17 @@
 library(dplyr)
 
-data <- read.csv("all_peaklist.csv")
+data <- read.csv("positive_featurelist.csv")
 ppm.limit <- 2
+data$mass <- data$mz - 1.007276
+
+#read phenodata
+PhD <- data.frame(read.csv("PhDP.csv"))
+fNames <- as.vector(PhD$X)
+fNames <- gsub("-", "\\.", fNames)
+fSamples <- as.vector(PhD$class)
+fSamples <- gsub("-", "\\.", fSamples)
+grs <- unique(fSamples)
+
 
 sup <- as.matrix(select(data, mz, mass))
 
@@ -44,38 +54,22 @@ colnames(KEGG_comp) <- header
 KEGG_comparison <- data.frame(KEGG_comp[-1, ])
 KEGG_int <- merge(KEGG_comparison, data, by = "mz")
 
-KEGG_int_for_pathview <- filter(KEGG_int, KEGG.ID != "no match")
-KEGG_int_for_pathview_2 <- select(KEGG_int_for_pathview, KEGG.ID, MW1a:MW8c)
-KEGG_int_for_pathview_2 <- distinct(KEGG_int_for_pathview_2, KEGG.ID)
-rownames(KEGG_int_for_pathview_2) <- KEGG_int_for_pathview_2$KEGG.ID
-KEGG_int_for_pathview_2$KEGG.ID <- NULL
-# KEGG_int_for_pathview_2$D1 <- apply(select(KEGG_int_for_pathview_2, D1.r001:D1.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$D2 <- apply(select(KEGG_int_for_pathview_2, D2.r001:D2.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$D3 <- apply(select(KEGG_int_for_pathview_2, D3.r001:D3.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$D4 <- apply(select(KEGG_int_for_pathview_2, D4.r001:D4.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$D5 <- apply(select(KEGG_int_for_pathview_2, D5.r001:D5.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$D6 <- apply(select(KEGG_int_for_pathview_2, D6.r001:D6.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$DS1 <- apply(select(KEGG_int_for_pathview_2, DS1.r001:DS1.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$ID1 <- apply(select(KEGG_int_for_pathview_2, ID1.r001:ID1.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$N1 <- apply(select(KEGG_int_for_pathview_2, N1.r001:N1.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$N2 <- apply(select(KEGG_int_for_pathview_2, N2.r001:N2.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$N3 <- apply(select(KEGG_int_for_pathview_2, N3.r001:N3.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$N4 <- apply(select(KEGG_int_for_pathview_2, N4.r001:N4.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$N5 <- apply(select(KEGG_int_for_pathview_2, N5.r001:N5.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$N6 <- apply(select(KEGG_int_for_pathview_2, N6.r001:N6.r003), 1, function(x) mean(x))
-# KEGG_int_for_pathview_2$NS1 <- apply(select(KEGG_int_for_pathview_2, NS1.r001:NS1.r003), 1, function(x) mean(x))
+pathview1 <- filter(KEGG_int, KEGG.ID != "no match")
+pathview2 <- pathview1[, c("KEGG.ID", fNames)]
+pathview2 <- distinct(pathview2, KEGG.ID)
+rownames(pathview2) <- pathview2$KEGG.ID
+pathview2$KEGG.ID <- NULL
 
-KEGG_int_for_pathview_2$MW1 <- apply(select(KEGG_int_for_pathview_2, MW1a:MW1c), 1, function(x) mean(x))
-KEGG_int_for_pathview_2$MW2 <- apply(select(KEGG_int_for_pathview_2, MW2a:MW2c), 1, function(x) mean(x))
-KEGG_int_for_pathview_2$MW3 <- apply(select(KEGG_int_for_pathview_2, MW3a:MW3c), 1, function(x) mean(x))
-KEGG_int_for_pathview_2$MW4 <- apply(select(KEGG_int_for_pathview_2, MW4a:MW4c), 1, function(x) mean(x))
-KEGG_int_for_pathview_2$MW5 <- apply(select(KEGG_int_for_pathview_2, MW5a:MW5c), 1, function(x) mean(x))
-KEGG_int_for_pathview_2$MW6 <- apply(select(KEGG_int_for_pathview_2, MW6a:MW6c), 1, function(x) mean(x))
-KEGG_int_for_pathview_2$MW7 <- apply(select(KEGG_int_for_pathview_2, MW7a:MW7c), 1, function(x) mean(x))
-KEGG_int_for_pathview_2$MW8 <- apply(select(KEGG_int_for_pathview_2, MW8a:MW8c), 1, function(x) mean(x))
-KEGG_int_for_pathview_2 <- select(KEGG_int_for_pathview_2, MW1:MW8)
-KEGG_int_for_pathview_norm <- data.frame(apply(KEGG_int_for_pathview_2, 1, function(x) -1+2*(x-min(x))/(max(x)-min(x))))
-KEGG_int_for_pathview_matrix <- t(as.matrix(KEGG_int_for_pathview_norm))
-head(KEGG_int_for_pathview_matrix)
-#KEGG_int_for_pathview_matrix2$kegg.names <- rownames(KEGG_int_for_pathview_matrix2) 
-write.csv(KEGG_int_for_pathview_matrix, "KEGG_comp.csv")
+for (i in 1:length(grs)) {
+        pathview2[[paste(grs[[i]])]] <- apply(pathview2[,grepl(grs[[i]], colnames(pathview2))], 1, mean)
+}
+
+pathview3 <- pathview2[, grs]
+pv_norm <- data.frame(apply(pathview3, 1, function(x) -1+2*(x-min(x))/(max(x)-min(x))))
+pv_matrix <- t(as.matrix(pv_norm))
+head(pv_matrix)
+
+#get csv files
+write.csv(pathview1, "KEGG_comp_full.csv")
+write.csv(pathview3, "KEGG_comp_short.csv")
+write.csv(pv_matrix, "KEGG_comp_short_norm.csv")
