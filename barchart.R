7d1010c037bb1226bbaa30edb601b0202f9dcd48
Egemen Aydin <Egemen Aydin>
2016-08-03 10:45:34 +0300

    added summary statistics and errorbars

--- barchart.R
+++ barchart.R
@@ -1,11 +1,22 @@
 library(ggplot2)
 library(dplyr)
+source("~/mm/summarySEwithin.R")
+source("~/mm/normDataWithin.R")
+source("~/mm/summarySE.R")
 d1 <- read.csv("nano-sentetik-negatif.csv")
 d2 <- reshape2::melt(d1, id = c("Malzeme", "pH", "Süre"))
+#when there are replicates use this for summary statistics
+d3 <- summarySEwithin(d2, measurevar = "value", withinvars = c("pH", "Süre", "Malzeme", "variable"), idvar = "Malzeme")
 
-p1 <- plyr::dlply(d2, "variable", function(x){
+p1 <- plyr::dlply(if (exists("d3")) {
+        .data = d3      
+  } else {
+          .data = d2  
+  },
+        "variable", function(x){
         ggplot(x, aes(x = Malzeme, y = value, fill = Süre)) +
                 geom_bar(position = position_dodge(),stat = "identity") +
+                geom_errorbar(position = position_dodge(), aes(ymin = value - sd, ymax = value + sd))+
                 facet_grid(~pH, labeller = labeller(pH = label_both))+
                 ylab("C/C0") +
                 #ggtitle(p1$data$variable) +
