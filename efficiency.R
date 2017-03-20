61d7e13d26fd06a87489add8c6ec950038de9d21
Egemen Aydin <egemenaydin1@gmail.com>
2017-03-20 11:10:29 +0300

    collect concentration data

--- efficiency.R
+++ efficiency.R
@@ -1,21 +1,24 @@
 source("~/mm/summarySEwithin.R")
 source("~/mm/normDataWithin.R")
 source("~/mm/summarySE.R")
-d1 <- read.csv("tum_maddeler_sadece_tesis.csv")
+d1 <- read.csv("tesis.csv")
 d2 <- reshape2::melt(d1, id = c("Numune"))
 #when there are replicates use this for summary statistics
 #d3 <- summarySEwithin(d1, measurevar = "concentration", withinvars = c("Numune", "variable", "value", "X"), idvar = "variable")
 #xlsx::write.xlsx(d3, "measurement_results.xlsx")
-tc <- dplyr::filter(d2, Numune == c("Tesis Cikis", "Tesis Giris"))
-tc2 <- summarySEwithin(tc, measurevar = "value", withinvars = c("Numune", "variable"), idvar = "variable")
-tc3 <- dplyr::select(tc2, Numune, variable, value)
 
-efficiency <- plyr::ddply(tc, "variable", function (x ,col){
+tc <- summarySEwithin(d2, measurevar = "value", withinvars = c("Numune", "variable"), idvar = "Numune")
+tc2 <- dplyr::filter(tc, Numune == "Tesis Cikis" | Numune == "Tesis Giris")
+tc3 <- dplyr::select(tc2, Numune, variable, value, sd)
+colnames(tc3)[2] <- "Madde"
+
+efficiency <- plyr::ddply(tc3, "Madde", function (x ,col){
         c(
-               efficiency = (x[1,3] - x[2,3])*100/x[1,3] 
+               efficiency = (x[2,3] - x[1,3])*100/x[2,3] 
         )
         },
         "value"
         )
 colnames(efficiency) <- c("Madde", "Giderim verimi(%)")
-xlsx::write.xlsx(efficiency, file = "tuzla_giderim.xlsx")
+xlsx::write.xlsx(efficiency, file = "tuzla_kis_giderim.xlsx")
+xlsx::write.xlsx(tc3, file = "tuzla_kis_konsantrasyonlar.xlsx")
