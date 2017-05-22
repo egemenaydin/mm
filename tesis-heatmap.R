932f8c765192447a5f68a686f873c9e27042fdb7
Egemen Aydin <egemenaydin1@gmail.com>
2017-05-22 15:21:03 +0300

    removed unnecessary arguments

--- tesis-heatmap.R
+++ tesis-heatmap.R
@@ -1,24 +1,18 @@
 library(ggplot2)
 library(plyr)
-source("~/mm/summarySEwithin.R")
-source("~/mm/normDataWithin.R")
-source("~/mm/summarySE.R")
-d1 <- read.csv("tum_maddeler_ing.csv")
-d2 <- summarySEwithin(d1, measurevar = "value", withinvars = c("Numune" ,"variable"), idvar = "Numune")
-l2 <- sort(unique(d2$variable))
-
+d1 <- read.csv("tummaddeler_tuzla_kis.csv")
 l <- unique(d1$Numune)
 
-td2 <- transform(d2, Numune = ordered(Numune, l))
-td2 <- transform(td2, variable = ordered(variable, l2))
+td2 <- transform(d1, Numune = ordered(Numune, l))
+
 
 ggplot(td2, aes(x = Numune, y = variable)) + 
         geom_tile(aes(fill = log2(value))) + 
         geom_text(aes(label = round(td2$value, 0))) +
         scale_fill_gradient(low = "green", high = "red") +
         theme_minimal(base_size =14) +
-        xlab("WWTP Unit") +
-        ylab("Compound") +
-        theme(legend.position = "none")
+        xlab("") +
+        ylab("") +
+        theme(legend.position = "none", axis.text.x = element_text(angle = 45, hjust = 1))
 
-dev.print(png, "heatmap.png", height = 5, width = 10, res = 600, unit = "in")
+dev.print(png, "heatmap_tuzla_kis_tr.png", height = 5, width = 10, res = 600, unit = "in")
