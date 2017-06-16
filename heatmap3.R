fb2a3a6e5161b9ef8759a131753cc89650ebff45
egemenaydin <egemenaydin1@gmail.com>
2017-06-17 01:02:18 +0300

    check names and length df3 added

--- heatmap3.R
+++ heatmap3.R
@@ -1,4 +1,4 @@
-df <- read.csv("yuzeysel_su_CYA_biomass_norm.csv")
+df <- read.csv("MC_OP_identified2.csv", check.names = FALSE)
 df2 <- df[-1]
 
 df[df == 0] <- 0.5*min(df2[df2>0])
@@ -8,11 +8,11 @@ df3$Area <- df$Area
 
 cols <- colorRampPalette(c("green","red"))(8)
 
-gplots::heatmap.2(as.matrix(df3[,-11]), Rowv = T, Colv = T, 
+gplots::heatmap.2(as.matrix(df3[,-length(df3)]), Rowv = T, Colv = T, 
                   col = cols, scale = c("none"), key = F, key.title = "NULL", 
                   keysize = 1, density.info = "none", na.rm=T, cexCol = 1, 
                   cexRow = 1.2, lhei = c(1, 6), lwid = c (0.5, 4), 
                   trace = "none", margins = c(9, 15), labRow = df$Area)
 
-dev.print(png, file = "biomass_norm_heatmap2.png", height=9, width=12, res = 600,
+dev.print(png, file = "biomass_norm_heatmap3.png", height=9, width=12, res = 600,
           units = "in")
