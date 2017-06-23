80ee8f4c2177ed140349fa7056ff59eb777b2da5
Egemen Aydin <egemenaydin1@gmail.com>
2017-06-23 13:11:22 +0300

    cosmetic

--- tesis-heatmap.R
+++ tesis-heatmap.R
@@ -1,18 +1,18 @@
 library(ggplot2)
 library(plyr)
-d1 <- read.csv("tummaddeler_tuzla_kis.csv")
+d1 <- read.csv("olcum_sonuc_forheatmap_ing.csv")
 l <- unique(d1$Numune)
 
 td2 <- transform(d1, Numune = ordered(Numune, l))
 
 
-ggplot(td2, aes(x = Numune, y = variable)) + 
-        geom_tile(aes(fill = log2(value))) + 
-        geom_text(aes(label = round(td2$value, 0))) +
+ggplot(td2, aes(x = Numune, y = compound)) + 
+        geom_tile(aes(fill = log2(concentration))) + 
+        geom_text(aes(label = round(td2$concentration, 0))) +
         scale_fill_gradient(low = "green", high = "red") +
         theme_minimal(base_size =14) +
         xlab("") +
         ylab("") +
         theme(legend.position = "none", axis.text.x = element_text(angle = 45, hjust = 1))
 
-dev.print(png, "heatmap_tuzla_kis_tr.png", height = 5, width = 10, res = 600, unit = "in")
+dev.print(png, "heatmap_tuzla_ib_ing.png", height = 5, width = 10, res = 600, unit = "in")
