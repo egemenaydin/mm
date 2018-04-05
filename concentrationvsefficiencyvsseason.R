ef8baddd03d8797479d410eeac6740908a6f64f1
egemenaydin <egemenaydin1@gmail.com>
2018-04-05 10:11:01 -0400

    new

--- concentrationvsefficiencyvsseason.R
+++ concentrationvsefficiencyvsseason.R
@@ -1,16 +1,17 @@
 library(ggplot2)
 
-d1 <- read.csv("efficiencyvsconc.csv")
+d1 <- read.csv("efficiencyvsconc_all.csv")
 
 options(scipen=100000000)
 
-ggplot(d1, aes(x = Concentration, y= Compound, color = Efficiency, shape = Season)) +
-        geom_point(size = 5) +
+ggplot(d1, aes(x = Compound, y= Efficiency)) +
+        geom_boxplot(na.rm = T) +
+        geom_point(aes(color = Season, shape = WWTP), size = 5) +
         ylab("")+
-        xlab("Influent Concentration (ng/L)") +
-        scale_color_gradient(low = "green", high = "red") +
-        scale_x_log10(breaks = c(1, 10, 100, 1000, 10000, 100000)) +
+        xlab("Efficiency (%)") +
+        scale_color_brewer(palette = "Set1") +
+        #scale_x_log10(breaks = c(1, 10, 100, 1000, 10000, 100000)) +
         theme_bw(base_size =24)+
-        theme(axis.text.x = element_text(angle = 90, hjust = 1))
+        theme(axis.text.x = element_text(angle = 75, hjust = 1))
 
 dev.print(png, "ces.png",height = 8, width = 12, res = 600, unit = "in")
