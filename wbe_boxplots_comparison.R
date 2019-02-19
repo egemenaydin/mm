6730c1620537f7b581a0d2f82770ce34c1d85631
egemenaydin <egemenaydin1@gmail.com>
2019-02-19 11:19:09 -0500

    better colors smaller point size vertical axis text

--- wbe_boxplots_comparison.R
+++ wbe_boxplots_comparison.R
@@ -1,18 +1,19 @@
 library(ggplot2)
 library(RColorBrewer)
 
-dt <- read.csv("MTL_consumption_V2.csv")
+dt <- read.csv("MTL_SHB_consumption_V1.csv")
 
 dt_tidy <- reshape2::melt(dt, id = c("Date", "Flowrate", "P_BOD", "day", "week", "Location"))
 
 p1 <- plyr::dlply(dt_tidy, "variable", function(x){
         ggplot(x, aes(x = Location, y = value)) +
                 geom_boxplot(outlier.shape = NA) +
-                geom_jitter(aes(color = day), size = 4) +
-                scale_color_manual(values = c("#013220", "#E41A1C")) +
+                geom_jitter(aes(color = day), size = 3) +
+                scale_color_brewer(palette="Dark2") +
                 labs(x = "", y = "Consumption (mg/day/1000 inhabitants)") +
-                ggtitle(x$variable) +
-                theme_bw(base_size = 24)
+                ggtitle(x$variable)  +
+                theme_bw(base_size = 24) +
+                theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
 })
 
 x <- length(p1)
