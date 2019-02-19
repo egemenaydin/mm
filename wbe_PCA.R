6bf1f1716290f5c23c02d1743b035c2080c90ea9
egemenaydin <egemenaydin1@gmail.com>
2019-02-19 11:54:08 -0500

    added MTL PCA

--- wbe_PCA.R
+++ wbe_PCA.R
@@ -7,6 +7,7 @@ dt <- read.csv("MTL_SHB_consumption_V1.csv")
 
 row.names(dt) <- paste(dt$Date, dt$Location, sep = "_")
 
+
 dt_pca <- dplyr::select(dt, Cocaine:Codeine, Location)
 
 wbe.pca <- prcomp(dt_pca[, -length(dt_pca)],  scale = TRUE)
@@ -23,3 +24,18 @@ ggsave("2city_PCA.png", height = 8, width = 8, dpi = 600, unit = "in")
 fviz_pca_ind(wbe.pca, select.ind = list(contrib = 15), repel = T)
 
 ggsave("2city_PCA_contributers.png", height = 8, width = 8, dpi = 600, unit = "in")
+
+
+dt2 <- dplyr::filter(dt, Location == "MTL_South" | Location == "MTL_North")
+
+dt_pca2 <- dplyr::select(dt2, Cocaine:Codeine, Location)
+
+wbe.pca2 <- prcomp(dt_pca2[, -length(dt_pca)],  scale = TRUE)
+
+fviz_pca_biplot(wbe.pca2, label="var", habillage=dt_pca2$Location,
+                addEllipses=TRUE, ellipse.level=0.95, pointsize = 2,
+                labelsize = 5, repel = T, invisible = "quali") + 
+        scale_color_brewer(palette="Dark2") +
+        theme_minimal(base_size = 18)
+
+ggsave("MTL_PCA.png", height = 8, width = 8, dpi = 600, unit = "in")
