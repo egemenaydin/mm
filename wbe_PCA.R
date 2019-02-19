177e5647606f961d621078e26b498db4a4e11152
egemenaydin <egemenaydin1@gmail.com>
2019-02-19 11:10:50 -0500

    better habillage colors smaller point size

--- wbe_PCA.R
+++ wbe_PCA.R
@@ -3,7 +3,7 @@ library(RColorBrewer)
 library(FactoMineR)
 library(factoextra)
 
-dt <- read.csv("MTL_consumption_V2.csv")
+dt <- read.csv("MTL_SHB_consumption_V1.csv")
 
 row.names(dt) <- paste(dt$Date, dt$Location, sep = "_")
 
@@ -12,14 +12,14 @@ dt_pca <- dplyr::select(dt, Cocaine:Codeine, Location)
 wbe.pca <- prcomp(dt_pca[, -length(dt_pca)],  scale = TRUE)
 
 fviz_pca_biplot(wbe.pca, label="var", habillage=dt_pca$Location,
-             addEllipses=TRUE, ellipse.level=0.95, pointsize = 3,
+             addEllipses=TRUE, ellipse.level=0.95, pointsize = 2,
              labelsize = 5, repel = T) + 
-        scale_color_brewer(palette="Set1") +
-        theme_bw(base_size = 18)
+        scale_color_brewer(palette="Dark2") +
+        theme_minimal(base_size = 18)
 
-ggsave("MTL_PCA.png", height = 8, width = 8, dpi = 600, unit = "in")
+ggsave("2city_PCA.png", height = 8, width = 8, dpi = 600, unit = "in")
 
 
 fviz_pca_ind(wbe.pca, select.ind = list(contrib = 15), repel = T)
 
-ggsave("MTL_PCA_contributers.png", height = 8, width = 8, dpi = 600, unit = "in")
+ggsave("2city_PCA_contributers.png", height = 8, width = 8, dpi = 600, unit = "in")
